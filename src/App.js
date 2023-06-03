5360f0fae76374f62c4306b79d917438cb464a5c
neelam <null.0990.null@gmail.com>
2023-06-03 20:32:03 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -10,20 +10,15 @@ import { Switch, Route } from "react-router-dom";
 import { HashRouter as Router } from "react-router-dom";
 import ScoreScreen from "./pages/ScoreScreen";
 import LoginPage from "./components/Login";
-// import NavBar from "./components/Navbar";
+
 import SignUpPage from "./components/SignUp";
 import Profile from "./components/Profile";
 import PageNotFound from "./pages/404Page";
-// import TestSummaryModal from "./components/test-summary-modal";
 
 function App() {
   return (
     <Router>
       <div className="App fs-5">
-        {/* <TestSummaryModal /> */}
-        {/* <NavBar /> */}
-        {/* <SignUpPage /> */}
-        {/* <LoginPage /> */}
         <Switch>
           <Route path="/" component={HomePage} exact />
           <Route path="/auth/register" component={SignUpPage} exact />
